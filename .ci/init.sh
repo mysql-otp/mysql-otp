7f52862c5f1e85f53a159ed7ac8cf0ee01ca0205
zmstone <zmstone@gmail.com>
2024-10-07 20:08:51 +0200

    Fix init script for CI

--- .ci/init.sh
+++ .ci/init.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-sudo mysql -uroot -e "CREATE USER otptest@localhost IDENTIFIED BY 'OtpTest--123';"
-sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO otptest@localhost;"
-sudo mysql -uroot -e "CREATE USER otptest2@localhost IDENTIFIED BY 'OtpTest2--123';"
-sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO otptest2@localhost;"
+set -euo pipefail
+
+MAX_ATTEMPTS=15
+attempt=0
+while ! mysql -uroot -e 'show databases;' >/dev/null 2>&1; do
+    attempt=$((attempt + 1))
+    echo "Waiting for MySQL... $attempt"
+    if [ "$attempt" -ge "$MAX_ATTEMPTS" ]; then
+        echo "Failed to connect to MySQL server after $MAX_ATTEMPTS attempts."
+        exit 1
+    fi
+    sleep 1
+done
+
+echo "Creating users for tests..."
+
+mysql -uroot -e "CREATE USER 'otptest'@'%' IDENTIFIED BY 'OtpTest--123';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO 'otptest'@'%';"
+mysql -uroot -e "CREATE USER 'otptest2'@'%' IDENTIFIED BY 'OtpTest2--123';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO 'otptest2'@'%';"
 
 # REQUIRE SSL must be specified only in CREATE USER in MySQL >= 8.0, only in GRANT in MySQL < 5.7;
 # MySQL 5.7 allows both variants.
-(sudo mysql -uroot -e "CREATE USER otptestssl@localhost IDENTIFIED BY 'OtpTestSSL--123' REQUIRE SSL;" &&
- sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO otptestssl@localhost;") ||
-(sudo mysql -uroot -e "CREATE USER otptestssl@localhost IDENTIFIED BY 'OtpTestSSL--123';" &&
- sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO otptestssl@localhost REQUIRE SSL;")
+(mysql -uroot -e "CREATE USER 'otptestssl'@'%' IDENTIFIED BY 'OtpTestSSL--123' REQUIRE SSL;" &&
+ mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO 'otptestssl'@'%';") ||
+(mysql -uroot -e "CREATE USER 'otptestssl'@'%' IDENTIFIED BY 'OtpTestSSL--123';" &&
+ mysql -uroot -e "GRANT ALL PRIVILEGES ON otptest.* TO 'otptestssl'@'%' REQUIRE SSL;")
+
+echo 'INIT-DONE'
